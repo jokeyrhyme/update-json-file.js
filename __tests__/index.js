f5bb9a1c220ec2a096d057494082fe1c7c00d545
Ron Waldon <jokeyrhyme@gmail.com>
2016-12-21 16:21:09 +1100

    add tests for updater that returns a Promise

--- __tests__/index.js
+++ __tests__/index.js
@@ -34,13 +34,46 @@ test('(sync) updater: (x) => x', () => {
     })
 })
 
-test('(sync) updater: adds property', () => {
+test('(sync) updater: add property', () => {
   const filePath = path.join(tempPath, 'sync-added.json')
   const data = { hello: 'world!' }
   const expected = { hello: 'world!', abc: 123 }
   const updater = (x) => {
     x.abc = 123
     return x
+  }
+  return Promise.resolve()
+    .then(() => pify(fs.writeFile)(filePath, JSON.stringify(data)))
+    .then(() => updateJsonFile(filePath, updater))
+    .then(() => pify(fs.readFile)(filePath))
+    .then((text) => JSON.parse(text))
+    .then((result) => {
+      expect(result).toEqual(expected)
+    })
+})
+
+test('(promise) updater: (x) => x', () => {
+  const filePath = path.join(tempPath, 'promise-unchanged.json')
+  const data = { hello: 'world!' }
+  const expected = data
+  const updater = (x) => Promise.resolve(x)
+  return Promise.resolve()
+    .then(() => pify(fs.writeFile)(filePath, JSON.stringify(data)))
+    .then(() => updateJsonFile(filePath, updater))
+    .then(() => pify(fs.readFile)(filePath))
+    .then((text) => JSON.parse(text))
+    .then((result) => {
+      expect(result).toEqual(expected)
+    })
+})
+
+test('(promise) updater: add property', () => {
+  const filePath = path.join(tempPath, 'promise-added.json')
+  const data = { hello: 'world!' }
+  const expected = { hello: 'world!', abc: 123 }
+  const updater = (x) => {
+    x.abc = 123
+    return Promise.resolve(x)
   }
   return Promise.resolve()
     .then(() => pify(fs.writeFile)(filePath, JSON.stringify(data)))
